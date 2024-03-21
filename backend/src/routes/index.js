d6d96a5bdb7753f09613dd7c1ec88b2180f6866f
Amit kumar <ak0638010@gmail.com>
2024-03-21 14:58:33 +0530

    final....

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const { donationData, getDonationData } = require("../controller");
- 
-
-
 
 const route = express.Router();
 
-route.post("/donations", donationData );
+route.post("/donations", donationData);
 route.get("/getDonationData", getDonationData);
- 
 
 module.exports = route;
