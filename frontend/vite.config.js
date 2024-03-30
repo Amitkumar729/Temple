1bd22f1220891dff2289e05ce2d6ed0bfb4d237a
Amit kumar <ak0638010@gmail.com>
2024-03-30 17:38:54 +0530

    htaccess file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs';
-import path from 'path';
+ 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    hmr: {
-      overlay: false,
-    },
-    strictPort: true,
-    port: 3000,
-     
-  },
 })
