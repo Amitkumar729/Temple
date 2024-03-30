7dbe99ec15ef8c568f49e268ed7f38a301d63923
Amit kumar <ak0638010@gmail.com>
2024-03-30 17:36:38 +0530

    vite config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    hmr: {
+      overlay: false,
+    },
+    strictPort: true,
+    port: 3000,
+     
+  },
 })
